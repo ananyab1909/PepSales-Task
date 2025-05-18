254058cb3d72dd7e74187f927438de24d07bd6a4
Ananya Biswas <153214834+ananyab1909@users.noreply.github.com>
2025-05-18 22:33:55 +0530

    Updated inapp_service.py

--- services/inapp_service.py
+++ services/inapp_service.py
@@ -1,9 +1,18 @@
+from tenacity import retry, stop_after_attempt, wait_exponential, retry_if_exception_type
+from pymongo.errors import PyMongoError
 from models.mongo import mongo
 
-def store_in_app(user_id, message):
+@retry(
+    stop=stop_after_attempt(3),
+    wait=wait_exponential(multiplier=1, min=2, max=10),
+    retry=retry_if_exception_type(PyMongoError),
+    reraise=True
+)
+def store_in_app(db, user_id, message):
     print(f"Storing IN-APP notification for {user_id}: {message}")
-    mongo.db.notifications.insert_one({
+    db.messages.insert_one({
         "user_id": user_id,
         "type": "inapp",
         "message": message
     })
+    print("In-app notification stored in DB.")
